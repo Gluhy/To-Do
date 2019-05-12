3504a21fd6c806a75bfa802a24a8ca99b41e6e43
Gluhy <46531470+Gluhy@users.noreply.github.com>
2019-05-12 18:49:34 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,19 +2,21 @@ const add_btn = document.querySelector("add_btn")
 const del_btn = document.querySelector("del_btn")
 let toDo = []
 const list = document.getElementById("list")
-const deleteBtnDiv = document.getElementById("deleteBtnDiv")
+const didBtnDiv = document.getElementById("didBtnDiv")
 let did_ = []
 
 document.getElementById("add_btn").addEventListener("click", add)
 document.getElementById("add_btn").addEventListener("click", clearAdd)
 //=====================================================================
-document.getElementById("del_btn").addEventListener("click", didF)
+document.getElementById("did_btn").addEventListener("click", didF)
+//=====================================================================
+document.getElementById("del_btn").addEventListener("click",toDelete)
 
 
 function show()
 {
     list.innerHTML = toDo
-    .map((element, index) => `${index + 1}. ${element} <input type="checkbox" id="checkbox${index}">`)
+    .map((element, index) => `${index + 1}. ${element} <input type="checkbox" id="checkboxToDo${index}">`)
         .join("<br>")
 }
 
@@ -30,9 +32,9 @@ function clearAdd()
     add_txt.value = ""
 }
 
-function toDelete()
+function moveToDid()
 {
-    toDo.forEach((_, i) => document.getElementById(`checkbox${i}`).checked && toDo.splice(i, 1))
+    toDo.forEach((_, i) => document.getElementById(`checkboxToDo${i}`).checked && toDo.splice(i, 1))
     show()
     didShow()
 }
@@ -48,16 +50,21 @@ function btnAdd()
 
 function didF()
 {
-    toDo.forEach((a, i) => document.getElementById(`checkbox${i}`).checked && did_.push(a))
+    toDo.forEach((a, i) => document.getElementById(`checkboxToDo${i}`).checked && did_.push(a))
     console.log(did_)
-    toDelete()
+    moveToDid()
 }
 
 function didShow()
 {
     const done = document.getElementById("done")
     done.innerHTML = did_
-    .map((element, index) => `${index + 1}. <strike>${element}</strike>`)
+    .map((element, index) => `${index + 1}. <strike>${element}</strike> <input type = "checkbox" id = "checkboxDid${index}">`)
     .join("</br>")
 }
 
+function toDelete()
+{
+    did_.forEach((_, index) => document.getElementById(`checkboxDid${index}`).checked && did_.splice(index, 1))
+    didShow()
+}
